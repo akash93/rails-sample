17f958422ef99751de8f785daf14cd9b092beb98
Akash Khan <akashkhan25@gmail.com>
2016-11-29 11:03:00 +0800

    Modified contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post 'contact', to: 'contacts#process_form'
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
 end
